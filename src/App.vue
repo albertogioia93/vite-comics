bca0197ac124f9c886d99d631f3064e0d3b7ef52
albertogioia93 <albertogioia@hotmail.it>
2023-03-23 16:49:31 +0100

    creazione componente + import in app.vue

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,14 @@
 <script>
-
+import AppHeader from './components/AppHeader.vue';
+export default {
+  components: {
+    AppHeader
+  }
+}
 </script>
 
 <template>
-  
+  <AppHeader></AppHeader>
 </template>
 
 <style scoped>
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,16 @@
+<script>
+export default {
+    name: 'header',
+    data (){
+        return {
+            message: 'ciao'
+        }
+    }
+}
+</script>
+
+<template>
+    <h1>{{ message }}</h1>
+</template>
+
+<style scoped></style>
