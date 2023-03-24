a2b08e3f0ca3efd7e70faf863c313de166c0e9f5
albertogioia93 <albertogioia@hotmail.it>
2023-03-24 20:33:44 +0100

    creazione props e inseriti nel componente generico

--- src/components/CardElement.vue
+++ src/components/CardElement.vue
@@ -1,13 +1,29 @@
 <script>
 export default {
-    name: 'Card'
+    name: 'Card',
+    props: {
+        img: String,
+        title: String
+    }
 }
 </script>
 
 <!-- CREAZIONE DI UN COMPONENTE GENERICO. I DATI LI LASCIO IN ComicsList.vue -->
-<template>
+<!-- <template>
     <div class="card">
         <div>img</div>
         <h3>titolo</h3>
     </div>
+</template> -->
+
+
+<!-- CREAZIONE DI UN COMPONENTE GENERICO DOPO AVER STABILITO I PROPS -->
+
+<template>
+    <div class="card">
+        <div><img :src="img" alt=""></div>
+        <h3>{{ title }}</h3>
+    </div>
 </template>
+
+<!-- PER POPOLARE QUESTI COMPONENTI BISOGNA ANDARE AL PADRE (IN QUESTO CASO ComicsList.vue) -->
